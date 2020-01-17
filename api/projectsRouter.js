0b1c6d7fd2fc23301dfd15bc40b745b3456e3cd7
josh Hill <Josh_hill15@me.com>
2020-01-17 12:29:27 -0500

    delete/put done

--- api/projectsRouter.js
+++ api/projectsRouter.js
@@ -2,7 +2,6 @@ const router = require("express").Router()
 const Projects = require("../data/helpers/projectModel")
 
 router.get("/", (req,res) => {
-    console.log('hello')
     Projects.get()
         .then(projects => res.status(200).json(projects))
         .catch(err => {
@@ -13,7 +12,6 @@ router.get("/", (req,res) => {
 
 router.post("/",(req,res) => {
     const { body } = req
-    console.log("hellllllo")
     Projects.insert(body)
         .then(newProject => res.status(200).json(newProject))
         .catch(err => {
@@ -23,7 +21,24 @@ router.post("/",(req,res) => {
 })
 
 router.delete("/:id", (req,res) => {
-    
+    const { id } = req.params
+    Projects.remove(id)
+        .then(deleted => res.status(200).json(deleted))
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({error: "can't find project to delete"})
+        })
+})
+
+router.put("/:id", (req,res) => {
+    const { id } = req.params
+    const { body } = req
+    Projects.update(id,body)
+        .then(updated => res.status(200).json(updated))
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({error: "can't find project to update"})
+        })
 })
 
 module.exports = router
